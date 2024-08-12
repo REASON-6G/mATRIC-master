a0eb69885c6b506e1e571359f396a87b4f4713ff
tom-bower <bower.t@protonmail.com>
2024-08-12 14:38:20 +0100

    modified dependencies for fastapi, added wmq dependencies

--- mo_agent_dev/app/wiremq/mapp_publisher.py
+++ mo_agent_dev/app/wiremq/mapp_publisher.py
@@ -3,11 +3,27 @@
 from matching_service_client import MSClient
 from app.config import settings
 
+
+ms_config = {
+    "name": "mapp_publisher",
+    "auth_url": "http://localhost:8080",
+    "realm": "reason-dev",
+    "certs_path": "protocol/openid-connect/certs",
+    "resource": "matching-service",
+    "client_secret_key": "KmBjqvrTA9PUDwIx9PObKYnTKksTRLtf",
+    "username": "mo_test_mapp",
+    "password": "mapp@Bristol",
+    "host": "localhost",
+    "port": 15000,
+    "data_port": 15001,
+    "log_level": "info",
+}
+
 class MAppPublisher:
     def __init__(self):
-        self.client = MSClient(settings.ms_config)
+        self.client = MSClient(ms_config)
         self.client.run()
-        self.client.connect(settings.ms_config["host"], settings.ms_config["port"], block=True)
+        self.client.connect(ms_config["host"], ms_config["port"], block=True)
 
     def publish_data(self, topic: str, payload: dict):
         """Publish data to a given topic."""
