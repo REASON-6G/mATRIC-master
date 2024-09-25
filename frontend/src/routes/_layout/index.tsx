3f7983a3cdac13cb5c0dd51ce6149a566a0586b1
D Brown <browndj24@gmail.com>
2024-09-25 22:20:40 +0100

    Add stats cards to dash

--- frontend/src/routes/_layout/index.tsx
+++ frontend/src/routes/_layout/index.tsx
@@ -1,9 +1,10 @@
-import { Box, Container, Text } from "@chakra-ui/react"
+import { Box, Card, Container, Text } from "@chakra-ui/react"
 import { createFileRoute } from "@tanstack/react-router"
 
 import useAuth from "../../hooks/useAuth"
 import Hero from "../../components/Common/Hero.tsx";
 import HeroHeader from "../../components/Common/HeroHeader.tsx";
+import NumberAccessPoints from "../../components/AccessPoint/NumberAccessPoints.tsx";
 
 export const Route = createFileRoute("/_layout/")({
   component: Dashboard,
@@ -24,7 +25,17 @@ function Dashboard() {
           <HeroHeader />
         </Box>
         <Box pt={6} m={4}>
-          <Hero />
+          <Card p={1} borderStyle={"inset"}>
+            <Hero />
+          </Card>
+        </Box>
+        <Box>
+          <Card p={4} m={2} borderStyle={"inset"}>
+            <NumberAccessPoints label={"Available Access points"} number={100} />
+          </Card>
+          <Card p={4} m={2} borderStyle={"inset"}>
+            <NumberAccessPoints label={"Available Agents"} number={100} />
+          </Card>
         </Box>
       </Container>
     </>
