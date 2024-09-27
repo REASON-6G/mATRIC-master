5708470b9b7c47e71f297e64fc99d7123fc91090
D Brown <browndj24@gmail.com>
2024-09-27 12:35:19 +0100

    frontend test config, add dash mainmap render test

---
+++ frontend/__tests__/MapMain.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import MapMain from '../src/components/Map/MapMain';
+
+describe('MapMain Component', () => {
+    const initialViewState = {
+        latitude: 51.4585,
+        longitude: 2.6022,
+        zoom: 10,
+    };
+
+    test('renders MapMain component correctly', () => {
+        render(<MapMain initialViewState={initialViewState} />);
+
+        // Check if the map container is rendered
+        const mapContainer = screen.getByRole('region');
+        expect(mapContainer).toBeInTheDocument();
+    });
+});
