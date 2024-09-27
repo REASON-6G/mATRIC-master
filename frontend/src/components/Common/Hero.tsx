029f45b3943780f0ce094427ee2186f6dd87e224
D Brown <browndj24@gmail.com>
2024-09-27 17:27:42 +0100

    Add fullscreen control to map
    Click button to make map full screen

--- frontend/src/components/Common/Hero.tsx
+++ frontend/src/components/Common/Hero.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useState } from "react";
+import { FullscreenControl, Popup } from "react-map-gl";
 import {
     Flex,Stack,
 } from '@chakra-ui/react'
@@ -8,6 +10,7 @@ import MapMain from "../Map/MapMain.tsx";
 
 export default function Hero() {
     const initialViewState = { latitude: 51.4492, longitude: -2.5813, zoom: 3 }
+    // const [showPopup, setShowPopup] = useState<boolean>(true);
 
     const markers = [
         { latitude: 51.4492, longitude: -2.5813 }, // Bristol
@@ -17,6 +20,12 @@ export default function Hero() {
 
     const handleMarkerClick = (latitude: number, longitude: number) => {
         console.log(`Marker clicked at latitude: ${latitude}, longitude: ${longitude}`);
+        // {showPopup && (
+        //     <Popup longitude={-2.5813} latitude={51.4492}
+        //            anchor="bottom"
+        //            onClose={() => setShowPopup(false)}>
+        //         You are here
+        //     </Popup>)}
     };
 
     return (
@@ -27,7 +36,9 @@ export default function Hero() {
                     initialViewState={initialViewState}
                     markers={markers}
                     onMarkerClick={handleMarkerClick}
-                />
+                >
+                    <FullscreenControl />
+                </MapMain>
             </Flex>
         </Stack>
     )
