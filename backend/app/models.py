858f662da13fdf25e64064099670b87fb9e1355d
Bruce James <bruce.james@bristol.ac.uk>
2025-01-31 14:42:17 +0000

    Some fixes; use mo's models

--- backend/app/models.py
+++ backend/app/models.py
@@ -117,71 +117,7 @@ class ThirdPartyApp(BaseModel):
         orm_mode = True
 
 
-class UpdatePassword(SQLModel):
-    current_password: str = Field(min_length=8, max_length=40)
-    new_password: str = Field(min_length=8, max_length=40)
-
-
-# Database model, database table inferred from class name
-class User(UserBase, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    hashed_password: str
-    items: list["Item"] = Relationship(back_populates="owner")
-
-
-# Properties to return via API, id is always required
-class UserPublic(UserBase):
-    id: int
-
-
-class UsersPublic(SQLModel):
-    data: list[UserPublic]
-    count: int
-
-
-# Shared properties
-class ItemBase(SQLModel):
-    title: str = Field(min_length=1, max_length=255)
-    description: str | None = Field(default=None, max_length=255)
-    # supported_commands: list[str] | None = Field(default=None)
-
-
-# Properties to receive on item creation
-class ItemCreate(ItemBase):
-    title: str = Field(min_length=1, max_length=255)
-
-
-# Properties to receive on item update
-class ItemUpdate(ItemBase):
-    title: str | None = Field(default=None, min_length=1, max_length=255)  # type: ignore
-
-
-# Database model, database table inferred from class name
-class Item(ItemBase, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    title: str = Field(max_length=255)
-    owner_id: int | None = Field(default=None, foreign_key="user.id", nullable=False)
-    owner: User | None = Relationship(back_populates="items")
-
-
-# Properties to return via API, id is always required
-class ItemPublic(ItemBase):
-    id: int
-    owner_id: int
-
-
-class ItemsPublic(SQLModel):
-    data: list[ItemPublic]
-    count: int
-
-
-# Generic message
-class Message(SQLModel):
-    message: str
-
-
-# JSON payload containing access token
-class Token(SQLModel):
+class Token(BaseModel):
     access_token: str
     token_type: str
     expires_in: int
